8f9110251c6fe242d50f2e6897cef6b32bffc8fa
Jahlilah <jbachou@students.kennesaw.edu>
2026-04-01 18:30:51 -0400

    Clean up header comments in program_b.py
    Removed unnecessary header comments to streamline the code.

--- program_b.py
+++ program_b.py
@@ -8,16 +8,6 @@
 # to Program A.
 # Resources Used: Class notes, lecture examples, and general debugging 
 
-# Program Name: program_b.py
-# Course: IT3883/Section XXX
-# Student Name: Jaleelah Bachou
-# Assignment Number: Assignment 4
-# Due Date: xx/xx/20XX
-# Purpose: This program waits for a connection from Program A, receives a
-# string, changes the string to uppercase, prints it, and sends it back
-# to Program A.
-# Resources Used: Class notes, lecture examples, and general debugging help.
-
 import socket
 
 # Store the host IP address and port number
