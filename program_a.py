de319415779c4dae78a411772961e519e22cdbf5
Jahlilah <jbachou@students.kennesaw.edu>
2026-04-01 18:15:56 -0400

    Add header comments to program_a.py
    Added header comments with program details and purpose.

--- program_a.py
+++ program_a.py
@@ -1,3 +1,13 @@
+# Program Name: program_a.py
+# Course: IT3883/Section 01
+# Student Name: Jaleelah Bachou
+# Assignment Number: Assignment 4
+# Due Date: 04/01/2026
+# Purpose: This program asks the user to enter a string, sends that string
+# to Program B through a socket connection, waits for the response, and
+# prints the returned message to the screen.
+# Resources Used: Class notes, lecture examples, and general debugging
+
 import socket
 
 server_ip = "127.0.0.1"
